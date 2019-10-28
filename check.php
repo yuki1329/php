160e8807609cc603078163202514876eeed4659e
Yuki Takahatake <yuki12152124@gmail.com>
2019-10-28 09:30:47 +0800

    修正　課題

--- check.php
+++ check.php
@@ -9,18 +9,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 }
     $nickname = $_POST['nickname'];
     // echo $nickname;
-    
+
     $email = $_POST['email'];
     // echo $email;
 
     $content = $_POST['content'];
     // echo $content;
 
+    $content2 = $_POST['content2'];
+
+    $age = $_POST['age'];
+
     if($nickname==''){
-        $nickname_result='ニックネームが入力されていません。';
+        $nickname_result='氏名が入力されていません。';
     }else{
-        $nickname_result='ようこそ' . $nickname . '様';
+        $nickname_result='氏名:' . $nickname;
     }
+
+    if($age==''){
+        $age_result='年齢が入力されていません。';
+    }else{
+        $age_result='年齢' . $age;
+    }
+
 
     if($email==''){
         $email_result='メールアドレスが入力されていません。';
@@ -34,6 +45,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $content_result='お問い合わせ内容:' . $content;
     }
 
+    if($content2==''){
+        $content2_result='お問い合わせ内容が入力されていません。';
+    }else{
+        $content2_result='お問い合わせ内容:' . $content2;
+    }
+
     require_once('function.php');
     ?>
     <!DOCTYPE html>
@@ -42,22 +59,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     <title>入力内容確認</title>
     <meta charset="utf-8">
 </head>
-<body>
+<body class="size2">
     <h1>入力内容確認</h1>
     <p><?php echo h($nickname_result); ?></p>
+    <p><?php echo h($age_result); ?></p>
+    <?php if(isset($_POST['sex'])) {
+        $sex = $_POST['sex'];
+        echo '性別：' . $sex . '<br>';
+    } else {
+        echo '性別が選択されていません。<br>';
+    } ?>
     <p><?php echo h($email_result); ?></p>
+    <?php if(isset($_POST['teacher'])) {
+        $teacher = $_POST['teacher'];
+        echo '講師：' . $teacher . '<br>';
+    } else {
+        echo '講師が選択されていません。<br>';
+    } ?><br>
+    <?php if(isset($_POST['cs'])) {
+        $cs = $_POST['cs'];
+        echo '満足度：' . $cs . '<br>';
+    } else {
+        echo '満足度が選択されていません。<br>';
+    } ?>
     <p><?php echo h($content_result); ?></p>
+    <p><?php echo h($content2_result); ?></p>
     <button type="button" onclick="history.back()">戻る</button>
     <form method="post" action="thanks.php">
     <input type="hidden" name="nickname" value="<?php echo $nickname;?>">
+    <input type="hidden" name="age" value="<?php echo $age;?>">
+    <input type="hidden" name="sex" value="<?php echo $sex;?>">
     <input type="hidden" name="email" value="<?php echo $email;?>">
+    <input type="hidden" name="teacher" value="<?php echo $teacher;?>">
+    <input type="hidden" name="cs" value="<?php echo $cs;?>">
     <input type="hidden" name="content" value="<?php echo $content;?>">
-    <!-- フォーム送信用のボタンじゃない時はtype="button"にする -->
-    <!-- フォーム送信用の時はtype="submit"にする -->
-    <!-- コロン構文 -->
-    <?php if ($nickname != '' && $email != '' && $content != ''):?>
+    <input type="hidden" name="content2" value="<?php echo $content2;?>">
         <button type="submit">OK</button>
-<?php endif;?>
 </form>
 </body>
 </html>
--- thanks.php
+++ thanks.php
@@ -3,19 +3,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   header('Location: index.html');
 }
 require_once('function.php');
+
 $nickname = $_POST['nickname'];
-// echo $nickname;
+    // echo $nickname;
+    
+    $email = $_POST['email'];
+    // echo $email;
 
-$email = $_POST['email'];
-// echo $email;
+    $content = $_POST['content'];
+    // echo $content;
 
-$content = $_POST['content'];
-// echo $content;
+    $content2 = $_POST['content2'];
+    
+    $age = $_POST['age'];
+    
+    $teacher = $_POST['teacher'];
+
+    $sex = $_POST['sex'];
+
+    $cs = $_POST['cs'];
+
+
 require_once('function.php');
 require_once('dbconnect.php');
 
-$stmt = $dbh->prepare('INSERT INTO surveys (nickname,email,content)VALUES(?,?,?)');
-$stmt->execute([$nickname,$email,$content]);
+ $stmt = $dbh->prepare('INSERT INTO surveys(nickname,age,sex,email,teacher,cs,content,content2)VALUES(?,?,?,?,?,?,?,?)');
+ $stmt->execute([$nickname,$age,$sex,$email,$teacher,$cs,$content,$content2]);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -23,10 +36,15 @@ $stmt->execute([$nickname,$email,$content]);
   <meta charset="UTF-8">
   <title>送信完了</title>
 </head>
-<body>
+<body class="size3">
   <h1>お問い合わせありがとうございました！</h1>
-  <p><?php echo h($nickname); ?></p>
-  <p><?php echo h($email); ?></p>
-  <p><?php echo h($content); ?></p>
+  <p><?php echo h('氏名:' . $nickname); ?></p>
+  <p><?php echo h('年齢:' . $age); ?></p>
+  <p><?php echo h('メールアドレス:' . $email); ?></p>
+  <p><?php echo h('性別:' . $sex); ?></p>
+  <p><?php echo h('講師:' . $teacher); ?></p>
+  <p><?php echo h('満足度:' . $cs); ?></p>
+  <p><?php echo h('内容1:' . $content); ?></p>
+  <p><?php echo h('内容2:' . $content2); ?></p>
 </body>
 </html>
